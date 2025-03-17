00a89cdda7c21db9f0a217f0efa39b09d1590aed
Tarsmio <tarsmio@gmail.com>
2025-03-17 01:08:37 +0100

    Async launch

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { loadCommands, loadEvents } = require('./utils/loaders');
 const { ToornamentTokenGest } = require('./utils/ToornamenTokenGest');
 const { getParticipants } = require('./utils/toornamentUtils');
+const { fetchStages, fetchGroups } = require('./utils/matchUtils');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 
-client.commands = new Collection();
-client.participants = []
-client.args = process.argv.slice(2)
-client.toornamentToken = ToornamentTokenGest.getInstance()
+async function launch() {
 
-loadCommands(client)
-loadEvents(client)
+    client.commands = new Collection();
+    client.participants = []
+    client.args = process.argv.slice(2)
+    client.toornamentToken = ToornamentTokenGest.getInstance()
+    client.stagesAtLaunch = await fetchStages()
+    client.groupsAtLaunch = await fetchGroups()
 
-client.login(process.env.DISCORD_TOKEN);
+    loadCommands(client)
+    loadEvents(client)
+
+    client.login(process.env.DISCORD_TOKEN);
+}
+
+launch()
+
+module.exports = {
+    client
+}
