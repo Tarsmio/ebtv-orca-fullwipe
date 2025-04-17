bbb960af49d2beeeb2dc8714914a06c24c190b5d
Tarsmio <tarsmio@gmail.com>
2025-04-17 16:52:25 +0200

    Offline soon message

--- handler/commandInteraction.js
+++ handler/commandInteraction.js
@@ -1,4 +1,6 @@
-const {commandLog } = require("../utils/logs");
+const { EmbedBuilder } = require("discord.js");
+const { commandLog } = require("../utils/logs");
+const { randomFullwipeColor } = require("../utils/utilityTools");
 
 module.exports = async (interaction) => {
     const command = interaction.client.commands.get(interaction.commandName);
@@ -8,17 +10,38 @@ module.exports = async (interaction) => {
         return;
     }
 
-    if(command.info.rolePermission.length > 0){
+    if (command.info.rolePermission.length > 0) {
         const member = interaction.member
 
-        if(!command.info.rolePermission.some(roleId => member.roles.cache.has(roleId))) return interaction.reply({
-            ephemeral : true,
-            content : "Désolé tu n'as pas le droit d'utiliser cette commande !"
+        if (!command.info.rolePermission.some(roleId => member.roles.cache.has(roleId))) return interaction.reply({
+            ephemeral: true,
+            content: "Désolé tu n'as pas le droit d'utiliser cette commande !"
         })
     }
 
     try {
         await command.execute(interaction);
+
+        let endEmbed = new EmbedBuilder()
+            .setTitle("C'est fini ! :cry:")
+            .setDescription("Fullwipe 2025 c'est terminé ! Le bot seras bientôt hors ligne... Merci de l'avoir utiliser")
+            .setImage("attachment://end.jpg")
+            .setColor(randomFullwipeColor())
+            .setFooter({
+                text: "Fullwipe 2025",
+                iconURL: interaction.client.user.avatarURL({
+                    extension: "png",
+                    size: 64
+                })
+            })
+
+        await interaction.channel.send({
+            embeds: [endEmbed],
+            files: [{
+                name: "end.jpg",
+                attachment: `./images/endImage.jpg`
+            }]
+        })
     } catch (error) {
         console.error(error);
         if (interaction.replied || interaction.deferred) {
